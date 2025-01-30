8864a803d6c1a01cf15a5d2b809970fb60bd553e
Christopher Higgins <113563713+chiggi24@users.noreply.github.com>
2025-01-30 18:09:02 -0500

    Added connection my local mysql server
    Added connection my local mysql server with changes to how we connect locally.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -10,20 +10,21 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // For parsing JSON body
 
 // MySQL Connection
-//const db = mysql.createConnection({
-//    host: process.env.DB_HOST || 'localhost',
-//    user: process.env.DB_USER || 'root',
-//    password: process.env.DB_PASSWORD || '',
-//    database: process.env.DB_NAME || 'mydatabase'
-//});
-//
-//db.connect(err => {
-//    if (err) {
-//        console.error('Database connection failed:', err);
-//        return;
-//    }
-//    console.log('Connected to MySQL database');
-//});
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Database connection failed:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
 
 // Test Route
 app.get('/', (req, res) => {
