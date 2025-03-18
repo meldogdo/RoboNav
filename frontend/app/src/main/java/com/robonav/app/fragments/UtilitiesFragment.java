a6f35c6e5c112690a5a75f08fcc698045d67e320
BrysonCrook <bcrook4@uwo.ca>
2025-03-18 15:46:32 -0400

    smal import change

--- frontend/app/src/main/java/com/robonav/app/fragments/UtilitiesFragment.java
+++ frontend/app/src/main/java/com/robonav/app/fragments/UtilitiesFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
